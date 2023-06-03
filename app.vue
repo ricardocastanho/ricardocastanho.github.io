aebb55d3e47b77175595011af7084beb70cd147d
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-06-03 14:53:33 -0300

    Update UI

--- app.vue
+++ app.vue
@@ -1,22 +1,75 @@
 <template>
-  <div class="container">
+  <div
+    class="h-screen flex flex-col justify-center items-center space-x-3 space-y-3">
     <div>
-      <p>Hello</p>
-      <p>I</p>
-      <p>am</p>
-      <p>Ricardo</p>
-    </div>
-    <div class="punchline">
-      <p>FULLSTACK</p>
-      <br />
-      <p>DEVELOPER</p>
+      <div class="flex flex-row space-x-1 my-1">
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          Hello
+        </p>
+        <p class="text-lg lg:text-4xl animate-wave">👋</p>
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          I
+        </p>
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          am
+        </p>
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          Ricardo
+        </p>
+      </div>
+      <div class="flex flex-col space-y-1">
+        <p
+          class="font-lb text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter bg-primary text-background px-1 py-2 hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          FULLSTACK
+        </p>
+        <p
+          class="font-lb text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter bg-primary text-background px-1 py-2 hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          DEVELOPER
+        </p>
+      </div>
+      <div class="flex flex-row space-x-1 my-1 items-end justify-end">
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          Reach
+        </p>
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          me
+        </p>
+        <p
+          class="font-lb text-lg lg:text-4xl bg-secondary hover:italic hover:shadow-lg hover:-translate-x-2 hover:-translate-y-2 transition">
+          in:
+        </p>
+        <a href="https://github.com/ricardocastanho">
+          <Icon
+            name="uil:github"
+            class="text-lg lg:text-4xl hover:-translate-x-2 hover:-translate-y-2 transition">
+          </Icon>
+        </a>
+        <a href="https://linkedin.com/in/castanhoricardo">
+          <Icon
+            name="uil:linkedin"
+            class="text-lg lg:text-4xl hover:-translate-x-2 hover:-translate-y-2 transition">
+          </Icon>
+        </a>
+        <a href="mailto:ricardocastanho632@gmail.com">
+          <Icon
+            name="uil:mailbox"
+            class="text-lg lg:text-4xl hover:-translate-x-2 hover:-translate-y-2 transition">
+          </Icon>
+        </a>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 useHead({
-  title: "Ricardo Castanho",
+  title: "Ricardo Castanho - FullStack Developer",
   htmlAttrs: {
     lang: "pt-br",
   },
@@ -24,58 +77,8 @@ useHead({
     {
       name: "description",
       content:
-        "Ricardo Castanho is a software developer with 2 years of experience, ready to change the future.",
+        "I've been a FullStack Software Developer for 3 years and I'm ready to change the future.",
     },
   ],
 });
 </script>
-
-<style>
-body {
-  background: #ececed;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-p {
-  background: #f2f2f2;
-  color: #ff4a4a;
-  font-size: 42px;
-  line-height: 74px;
-  font-weight: 700;
-  margin: 0 5px 24px;
-  float: left;
-  padding: 10px;
-  margin: 0 5px 24px;
-  font-family: "Libre Baskerville", serif;
-}
-
-.punchline p {
-  background: #ff4a4a;
-  color: #f2f2f2;
-  font-size: 100px;
-  display: flex;
-  align-items: center;
-  justify-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-}
-
-a {
-  color: #adadad;
-  font-size: 25px;
-  text-decoration: none;
-  float: right;
-  font-family: "Libre Baskerville", serif;
-  line-height: 4;
-}
-
-a:hover {
-  color: #ff4a4a;
-  text-decoration: none;
-}
-</style>
