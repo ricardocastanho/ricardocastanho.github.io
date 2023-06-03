fd4b098a685d7a49421e948862f601923e5b907e
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-06-03 15:25:10 -0300

    Add PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
+import { pwa } from "./config/pwa";
+
 export default defineNuxtConfig({
-  modules: ["nuxt-icon"],
+  modules: ["nuxt-icon", "@vite-pwa/nuxt"],
+  pwa,
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
